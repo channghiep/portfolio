16fcb328eb7e8698d1e538d0ab1a7ed9c83d0d0a
channghiep <48778072+channghiep@users.noreply.github.com>
2022-12-23 16:10:43 -0800

    Portfolio MVP

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        background1: "#0A192F",
+        textcolor: "#58E3C4",
+        textcolor2: "#CCD6F6",
+        textcolor3: "#8892B0"
+      }
+    },
+  },
+  plugins: [],
+}
