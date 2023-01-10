9b1c1dee296b6b1d860305db25d9b33c23bc6996
channghiep <48778072+channghiep@users.noreply.github.com>
2023-01-10 10:59:27 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
       }
     },
     fontFamily:{
-      'sans': ["Calibre","Inter","San Francisco","SF Pro Text"],
+      'sans': ["Calibre","Sofia Sans","San Francisco","SF Pro Text"],
       'mono': ["SF Mono","Fira Code","Fira Mono","Roboto Mono", "monospace"]
     },
     boxShadow: {
